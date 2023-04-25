fb410b89bee7738fd7cef93d9977fb19e03bcf4a
erseco <erseco@users.noreply.github.com>
2023-04-25 19:58:34 +0000

    deploy: 19eec77fa08f04d5e6363b9bb0d4dd2bb3ae2c83

--- script.js
+++ script.js
@@ -6,100 +6,131 @@ const guessButton = document.getElementById('guessButton');
 const resultMessage = document.getElementById('resultMessage');
 const usedLettersDisplay = document.createElement('div');
 
-usedLettersDisplay.className = 'mt-3';
+usedLettersDisplay.className = 'usedLetters mt-3';
 wordDisplay.parentNode.insertBefore(usedLettersDisplay, wordDisplay.nextSibling);
 
 const urlParams = new URLSearchParams(window.location.search);
-const word = urlParams.get('palabra').toUpperCase();
-const wordLength = word.length;
-const maxGuesses = 6;
-let currentGuesses = 0;
-let correctGuesses = 0;
-let usedLetters = [];
+// const word = urlParams.get('palabra');
 
-function init() {
-  for (let i = 0; i < wordLength; i++) {
-    const letterBox = document.createElement('span');
-    letterBox.className = 'letter-box';
-    letterBox.innerText = '_';
-    wordDisplay.appendChild(letterBox);
+var word = urlParams.get("palabra");
+
+if (word) {
+
+  word = word.toUpperCase();
+
+  document.getElementById("enterWordContainer").style.display = "none";
+  document.getElementById("gameContainer").style.display = "block";
+
+  const wordLength = word.length;
+  const maxGuesses = 6;
+  let currentGuesses = 0;
+  let correctGuesses = 0;
+  let usedLetters = [];
+
+  function init() {
+    for (let i = 0; i < wordLength; i++) {
+      const letterBox = document.createElement('span');
+      letterBox.className = 'letter-box';
+      letterBox.innerText = '_';
+      wordDisplay.appendChild(letterBox);
+    }
+    drawHangmanBase();
   }
-  drawHangmanBase();
-}
 
-function drawHangmanBase() {
-  // Dibuja la base del ahorcado
-  ctx.beginPath();
-  ctx.moveTo(50, 230);
-  ctx.lineTo(100, 230);
-  ctx.moveTo(75, 230);
-  ctx.lineTo(75, 30);
-  ctx.lineTo(150, 30);
-  ctx.lineTo(150, 50);
-  ctx.stroke();
-}
-
-function drawHangmanBodyPart(part) {
-  ctx.beginPath();
-  switch (part) {
-    case 0: // Cabeza
-      ctx.arc(150, 70, 20, 0, Math.PI * 2);
-      break;
-    case 1: // Cuerpo
-      ctx.moveTo(150, 90);
-      ctx.lineTo(150, 150);
-      break;
-    case 2: // Brazo izquierdo
-      ctx.moveTo(150, 100);
-      ctx.lineTo(130, 120);
-      break;
-    case 3: // Brazo derecho
-      ctx.moveTo(150, 100);
-      ctx.lineTo(170, 120);
-      break;
-    case 4: // Pierna izquierda
-      ctx.moveTo(150, 150);
-      ctx.lineTo(130, 180);
-      break;
-    case 5: // Pierna derecha
-      ctx.moveTo(150, 150);
-      ctx.lineTo(170, 180);
-      break;
+  function drawHangmanBase() {
+    // Dibuja la base del ahorcado
+    ctx.beginPath();
+    ctx.moveTo(50, 230);
+    ctx.lineTo(100, 230);
+    ctx.moveTo(75, 230);
+    ctx.lineTo(75, 30);
+    ctx.lineTo(150, 30);
+    ctx.lineTo(150, 50);
+    ctx.stroke();
   }
-  ctx.stroke();
-}
 
-function checkLetter() {
-  const guessedLetter = letterInput.value.toUpperCase();
-  letterInput.value = '';
+  function drawHangmanBodyPart(part) {
+    ctx.beginPath();
+    switch (part) {
+      case 0: // Cabeza
+        ctx.arc(150, 70, 20, 0, Math.PI * 2);
+        break;
+      case 1: // Cuerpo
+        ctx.moveTo(150, 90);
+        ctx.lineTo(150, 150);
+        break;
+      case 2: // Brazo izquierdo
+        ctx.moveTo(150, 100);
+        ctx.lineTo(130, 120);
+        break;
+      case 3: // Brazo derecho
+        ctx.moveTo(150, 100);
+        ctx.lineTo(170, 120);
+        break;
+      case 4: // Pierna izquierda
+        ctx.moveTo(150, 150);
+        ctx.lineTo(130, 180);
+        break;
+      case 5: // Pierna derecha
+        ctx.moveTo(150, 150);
+        ctx.lineTo(170, 180);
+        break;
+    }
+    ctx.stroke();
+  }
 
-  if (guessedLetter && !usedLetters.includes(guessedLetter)) {
-    usedLetters.push(guessedLetter);
-    usedLettersDisplay.innerText = `Letras usadas: ${usedLetters.join(', ')}`;
+  function checkLetter() {
+    const guessedLetter = letterInput.value.toUpperCase();
+    letterInput.value = '';
 
-    if (word.includes(guessedLetter)) {
-      for (let i = 0; i < wordLength; i++) {
-        if (word[i] === guessedLetter) {
-          wordDisplay.children[i].innerText = guessedLetter;
-          correctGuesses++;
+    if (guessedLetter && !usedLetters.includes(guessedLetter)) {
+      usedLetters.push(guessedLetter);
+      usedLettersDisplay.innerText = `Letras usadas: ${usedLetters.join(', ')}`;
+
+      if (word.includes(guessedLetter)) {
+        for (let i = 0; i < wordLength; i++) {
+          if (word[i] === guessedLetter) {
+            wordDisplay.children[i].innerText = guessedLetter;
+            correctGuesses++;
+          }
         }
-      }
 
-      if (correctGuesses === wordLength) {
-        resultMessage.innerText = '¡Has ganado!';
-        guessButton.disabled = true;
-      }
-    } else {
-      drawHangmanBodyPart(currentGuesses);
-      currentGuesses++;
+        if (correctGuesses === wordLength) {
+          resultMessage.innerText = '¡Has ganado!';
+          guessButton.disabled = true;
+        }
+      } else {
+        drawHangmanBodyPart(currentGuesses);
+        currentGuesses++;
 
-      if (currentGuesses === maxGuesses) {
-        resultMessage.innerText = 'Has perdido';
-        guessButton.disabled = true;
+        if (currentGuesses === maxGuesses) {
+          resultMessage.innerText = 'Has perdido';
+          guessButton.disabled = true;
+        }
       }
     }
   }
+
+  guessButton.addEventListener('click', checkLetter);
+  init();
+
 }
 
-guessButton.addEventListener('click', checkLetter);
-init();
+
+
+document.getElementById("submitWord").addEventListener("click", (event) => {
+  const enterWord = document.getElementById("enterWord").value.trim();
+  if (enterWord) {
+      window.location.href = window.location.pathname + "?palabra=" + encodeURIComponent(enterWord);
+  }
+});
+
+
+document.getElementById("enterWord").addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        const enterWord = document.getElementById("enterWord").value.trim();
+        if (enterWord) {
+            window.location.href = window.location.pathname + "?palabra=" + encodeURIComponent(enterWord);
+        }
+    }
+});
